c1e42e8d7e745274c7528a29aa09b91c196e3014
Peter Max Guertler <coder@sonensei.ch>
2021-01-02 11:05:56 +0100

    Analyse tree with commit objects

--- run_test.py
+++ run_test.py
@@ -1,48 +1,29 @@
+#!/usr/bin/python3
+
 from src.ginsp_lib import *
 
-repo_path = '/home/franz/work/ginsp-sample'
-ri = RepoInfo(repo_path)
+repo_path = '../sample-repo-local'
+remote = 'origin'
 
-for b in ri.heads.locals.keys():
+
+repo = GitRepo(repo_path, remote)
+
+print("===== Repo Info for remote {} =====".format(remote))
+print("----- Branches -----")
+for b in repo.branches():
     print('  Branch {}'.format(b))
-    print(ri.find_release_local(b))
+print("----- Releases -----")
+for r in repo.releases():
+    print('  Release {}'.format(r))
+print("----- Feature Branches -----")
+for f in repo.feature_branches():
+    print('  Feature Branch {} in release {}'.format(f, repo.get_release(f)))
+print("----- The End -----")
 
-#print(ri.heads.inspect())
-#
-#mbl = ri.heads.max_local_branch_name_length()
-#
-#print('------ Consistency Check ------')
-#for b1 in ri.heads.locals.keys():
-#    for b2 in ri.heads.locals.keys():
-#        if b1 == b2:
-#            if ri.relations.is_before(b1, b2) or ri.relations.is_after(b1, b2):
-#                print("ERROR: Same is before or after")
-#        else:
-#            if ri.relations.is_before(b1, b2):
-#                if ri.relations.is_after(b1, b2):
-#                    print("ERROR: is before and after")
-#                if not ri.relations.is_after(b2, b1):
-#                    print("Error: Wrong inverse:")
-#            if ri.relations.is_after(b1, b2):
-#                if ri.relations.is_before(b1, b2):
-#                    print("ERROR: is after and before")
-#                if not ri.relations.is_before(b2, b1):
-#                    print("Error: Wrong inverse:")
-#            
-#print('------ Before / After Relations ------')
-#for r in ri.local_relations():
-#    a = r[0]
-#    b = r[1]
-#    if a == b:
-#        before = r[2]
-#        after = r[3]
-#        print("{} {} before {},  {} {} after {}".format(a.ljust(mbl, ' '), '   ' if before else 'not', b.ljust(mbl, ' '), a.ljust(mbl, ' '), '   ' if after else 'not', b.ljust(mbl, ' ')))
-#print()
-#for r in ri.local_relations():
-#    a = r[0]
-#    b = r[1]
-#    if a != b:
-#        before = r[2]
-#        after = r[3]
-#        print("{} {} before {},  {} {} after {}".format(a.ljust(mbl, ' '), '   ' if before else 'not', b.ljust(mbl, ' '), a.ljust(mbl, ' '), '   ' if after else 'not', b.ljust(mbl, ' ')))
-#
+print("===== INSPECTION =====")
+for sha1, commit in repo.commits_by_sha1.items():
+    print("{} : {} / {}".format(
+        sha1,
+        ", ".join(list(commit.terminators)),
+        ", ".join(list(commit.release_candidates))
+    ))
--- src/ginsp_lib.py
+++ src/ginsp_lib.py
@@ -10,9 +10,6 @@ def cmd(cmds):
         raise Exception("Failed to run '{}'".format(cmd))
     return out.decode("utf8")
 
-PREFIX_LOCAL = 'refs/heads/'
-PREFIX_REMOTE = 'refs/remotes/'
-
 class MapOfSet:
     def __init__(self):
         self.map = {}
@@ -25,174 +22,116 @@ class MapOfSet:
     def keys(self):
         return self.map.keys()
 
+    def items(self):
+        return self.map.items()
+
     def __repr__(self):
         return "MapOfSet{}".format(self.map)
 
-class Heads():
-    def __init__(self):
-        self.locals = {}
-        self.remotes = {}
+class Commit:
+    def __init__(self, repo, sha1):
+        self.repo = repo
+        self.sha1 = sha1
+        self.parents = None
+        self.release_candidates = set()
+        self.terminators = set()
+        self.later_releases = set()
+
+    def get_parents(self):
+        if self.parents is None:
+            self.parents = []
+            for l in cmd("git cat-file -p {}".format(self.sha1)).splitlines():
+                if l.startswith("parent "):
+                    parent_sha1 = l.split("parent ")[1]
+                    self.parents.append(self.repo.get_commit(parent_sha1))
+        return self.parents
+
+class GitRepo:
+    def __init__(self, repo_path, remote):
+        os.chdir(repo_path)
+        cmd('git fetch {} -t --prune'.format(remote))
+
+        self.commits_by_sha1 = {}
+
+        self.release_names = set()
+
+        # heads: key = tag/branch, value = sha1 of commit
+        self.heads = {}
+        branch_prefix = 'refs/remotes/{}/'.format(remote)
+        tag_prefix = 'refs/tags/'
         for l in cmd('git show-ref').splitlines():
             sha1, ref = l.split(' ')
-            if ref.startswith(PREFIX_LOCAL):
-                self.locals[ref[len(PREFIX_LOCAL):]] = sha1
-            elif ref.startswith('refs/remotes/'):
-                remote, branch = ref[len(PREFIX_REMOTE):].split('/')
-                self._add_remote(remote, branch, sha1)
+            if ref.startswith(branch_prefix):
+                self.heads[ref.split(branch_prefix)[1]] = sha1
+            elif ref.startswith(tag_prefix):
+                self.heads[ref.split(tag_prefix)[1]] = sha1
 
-    def branch_names(self):
-        all = set()
-        all.update(self.locals.keys())
-        for r in self.remotes.keys():
-            all.update(self.remotes[r].keys())
-        return all
+        terminated_releases = set()
+        terminators = []
+        for name, sha1 in self.heads.items():
+            if name.startswith('golive/'):
+                release = name.split('golive/')[1]
+                terminators.append( (release,sha1))
+                c = self.get_commit(sha1)
+                c.terminators.add(release)
+                terminated_releases.add(release)
 
-    def max_local_branch_name_length(self):
-        return max([len(b) for b in self.locals.keys()])
+        for name, sha1 in self.heads.items():
+            if name.startswith('golive/'):
+                self._spread_release(name.split('golive/')[1], sha1)
+        for head, sha1 in self.heads.items():
+            if head.startswith('dev/'):
+                self._spread_release(head.split('dev/')[1].split('.')[0], sha1)
+            elif name.startswith('release/'):
+                self._spread_release(head.split('release/')[1], sha1)
+            elif name.startswith('pre/'):
+                self._spread_release(head.split('pre/')[1], sha1)
 
-    def _add_remote(self, remote, branch, sha1):
-        if remote not in self.remotes:
-            self.remotes[remote] = {}
-        self.remotes[remote][branch] = sha1
+    def _spread_release(self, release, sha1):
+        if release not in self.release_names:
+            self.release_names.add(release)
+        commit = self.get_commit(sha1)
+        if len(commit.terminators) == 0 or release in commit.terminators:
+            commit.release_candidates.add(release)
+            for p in commit.get_parents():
+                self._spread_release(release, p.sha1)
+        else:
+            commit.later_releases.add(release)
 
-    def inspect(self):
-        lines = []
-        lines.append("CommitNet[")
-        lines.append("  Local:")
-        for l in sorted(self.locals.keys()):
-            lines.append("    {} : {}".format(l, self.locals[l][:8]))
-        lines.append("  Remote:")
-        for r in sorted(self.remotes.keys()):
-            lines.append("    {}".format(r))
-            rb = self.remotes[r]
-            for b in sorted(rb.keys()):
-                lines.append("      {} : {}".format(b, rb[b][:8]))
-        lines.append(']')
-        return '\n'.join(lines)
+    def get_commit(self, sha1):
+        if sha1 in self.commits_by_sha1:
+            return self.commits_by_sha1[sha1]
+        else:
+            c = Commit(self, sha1)
+            self.commits_by_sha1[sha1] = c
+            return c
 
+    def _find_release_candidates(self, cand, commit):
+        if len(commit.terminators) > 0:
+            cand.update(commit.later_releases)
+        elif len(commit.release_candidates) > 0:
+            cand.update(commit.release_candidates)
+        else:
+            for p in commit.get_parents():
+                self._find_release_candidates(cand, p)
 
-class RevLists:
-    def __init__(self):
-        self.data = {}
-    def get(self, sha1):
-        if sha1 not in self.data:
-            self.data[sha1] = cmd('git rev-list {}'.format(sha1)).splitlines()
-        return self.data[sha1]
+    def get_release(self, feature_branch):
+        sha1 = self.heads[feature_branch]
+        commit = self.get_commit(sha1)
+        release_candidates = set()
+        self._find_release_candidates(release_candidates, commit)
+        if len(release_candidates) == 1:
+            return list(release_candidates)[0]
+        else:
+            print("Can't tell release of {} from looking at Git, have to check Jira ({})".format(feature_branch, ", ".join(list(release_candidates))))
+            return None
 
-class RelKey:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def branches(self):
+        return self.heads.keys()
 
-    def inv(self):
-        return RelKey(self.b, self.a)
+    def feature_branches(self):
+        return [b for b in self.heads.keys() if b.startswith('feature/')]
 
-    def __eq__(self, other):
-        return self.a == other.a and self.b == other.b
+    def releases(self):
+        return sorted(list(self.release_names))
 
-    def __hash__(self):
-        return hash((self.a, self.b))
-
-    def __repr__(self):
-        return 'RelKey[{}, {}]'.format(self.a[:8], self.b[:8])
-
-class Relations:
-    def __init__(self, rev_lists):
-        self.rev_lists = rev_lists
-        self.before = {}
-        self.after = {}
-    
-    def is_before(self, a, b):
-        if a == b:
-            return False
-        return self.before[self._key(a, b)]
-
-    def is_after(self, a, b):
-        if a == b:
-            return False
-        return self.after[self._key(a, b)]
-
-    def _key(self, a, b):
-        k = RelKey(a, b)
-        if k not in self.before:
-            if k in self.after:
-                raise Exception('Nasty inconsistency (1): {}'.format(k))
-            self._calc(k)
-        if k not in self.after:
-            raise Exception('Nasty inconsistency (2): {}'.format(k))
-        return k
-
-    def _calc(self, key):
-        inv = key.inv()
-        found1 = False
-        for anc in self.rev_lists.get(key.a):
-            if key.b == anc:
-                self.after[key] = True
-                self.after[inv] = False
-                self.before[key] = False
-                self.before[inv] = True
-                found1 = True
-                break
-
-        found2 = False
-        for anc in self.rev_lists.get(key.b):
-            if key.a == anc:
-                if key in self.before or key in self.after:
-                    raise Exception("Nasty inconsistency (3) {}".format(key))
-                self.after[key] = True
-                self.after[inv] = False
-                self.before[key] = False
-                self.before[inv] = True
-                found2 = True
-                break
-        if not found1 and not found2:
-            self.after[key] = False
-            self.after[inv] = False
-            self.before[key] = False
-            self.before[inv] = False
-
-class RepoInfo:
-    def __init__(self, path):
-        os.chdir(path)
-        for r in cmd('git remote').splitlines():
-            cmd('git fetch {}'.format(r))
-        self.heads = Heads()
-        self.rev_lists = RevLists()
-        self.relations = Relations(self.rev_lists)
-
-    def local_relations(self):
-        result = []
-        for h1 in sorted(self.heads.locals.keys()):
-            h1_sha1 = self.heads.locals.get(h1)
-            for h2 in sorted(self.heads.locals.keys()):
-                h2_sha1 = self.heads.locals.get(h2)
-                result.append((h1, h2, self.relations.is_before(h1_sha1, h2_sha1), self.relations.is_after(h1_sha1, h2_sha1)))
-        return result
-
-    def _find_release_commits(self, heads_map):
-        release_prefix = 'release/'
-        dev_prefix = 'dev/'
-        release_candidates = MapOfSet()
-        for b in heads_map.keys():
-            if b.startswith(release_prefix):
-                release_name = b[len(release_prefix):]
-                release_candidates[release_name].add(heads_map[b])
-            if b.startswith(dev_prefix):
-                dev_branch_name = b[len(dev_prefix):]
-                release_name = dev_branch_name.split('.')[0]
-                release_candidates[release_name].add(heads_map[b])
-        return release_candidates
-
-    def _find_release(self, heads_map, branch_name):
-        release_commits = self._find_release_commits(heads_map)
-        branch_sha1 = heads_map[branch_name]
-        
-
-        
-    def find_release_local(self, branch_name):
-        h = self.heads.locals
-        return self._find_release(self.heads.locals, branch_name)
-
-    def find_release_remote(self, remote, branch_name):
-        return self._find_release(self.heads.remotes[remote], branch_name)
-        
